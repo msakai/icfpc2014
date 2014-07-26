7e70178d33077069ce23ef7f58c046e7b05776f5
Nobuo Yamashita <nobsun@sampou.org>
2014-07-26 15:59:30 +0900

    Virtual Machine

--- GHostCPU.hs
+++ GHostCPU.hs
@@ -1,5 +1,6 @@
 module GHostCPU where
 
+import Data.Bits
 import Data.Array.IArray
 import Data.Array.IO
 import Data.IORef
@@ -9,7 +10,8 @@ import Data.Word
 type Reg = Char
 
 data Arg
-  = ArgReg {-# UNPACK #-} !Reg
+  = ArgPC
+  | ArgReg {-# UNPACK #-} !Reg
   | ArgInd {-# UNPACK #-} !Reg
   | ArgConst {-# UNPACK #-} !Word8
   | ArgLoc {-# UNPACK #-} !Word8
@@ -53,19 +55,271 @@ data Inst
   | HLT
     -- ^ Halt execution of the GHC.
 
+type InstructionCounter = IORef Word16 
+type ProgramCounter     = IORef Word8
+type Registers   = IOArray Reg Word8
+type DataMemory  = IOArray Word8 Word8
+type CodeMemory  = Array Word8 Inst
+type InterruptHandler = Rgisters -> Registers
 
 data Machine
   = Machine
-  { mPC :: IORef Word8
-  , mRegs :: IOArray Reg Word8
-  , mDataMemory :: IOArray Word8 Word8
-  , mCodeMemory :: Array Word8 Inst
+  { mIC   :: InstructionCounter
+  , mPC   :: ProgramCounter
+  , mRegs :: Registers
+  , mDataMemory :: DataMemory
+  , mCodeMemory :: CodeMemory
+  , mIntHandler :: Array Word8 InterruptHandler
   }
+
+data Reason 
+   = ResHLT -- HLT
+   | ResErr -- Error
+   | ResLim -- Instruction Limit
+   | ResNon -- Not terminated
+   deriving (Show)
 
 newMachine :: [Inst] -> IO Machine
 newMachine prog = do
+  ic <- newIORef 0
   pc <- newIORef 0
   regs <- newArray ('A','H') 0
-  mem <- newArray (0,0xff) 0
+  mem  <- newArray (0,0xff) 0
   let code = array (0,0xff) $ zip [0..] (take 256 prog)
-  return $ Machine pc regs mem code
+  return $ Machine ic pc regs mem code
+
+runMachine :: Machine -> IO (Machine, Reason)
+runMachine m = do 
+  { mr@(m',r) <- instrCycle m
+  ; case r of
+      ResNon -> runMachine m'
+      _      -> return mr
+  }
+
+instrCycle :: Machine -> IO (Machine,Reason)
+instrCycle m@(Machine _ pc _ _ code) = do
+  { pc   <- readIORef pc
+  ; case code ! pc of
+      mov@(MOV _ _) -> execMov m mov
+      inc@(INC _ _) -> execInc m inc
+      dec@(DEC _ _) -> execDec m dec
+      add@(ADD _ _) -> execAdd m add
+      sub@(SUB _ _) -> execSub m sub
+      mul@(MUL _ _) -> execMul m mul
+      div@(DIV _ _) -> execDiv m div
+      and@(AND _ _) -> execAnd m and
+      or@(OR _ _)   -> execOr  m or
+      xor@(XOR _ _) -> execXor m xor
+      jlt@(JLT _ _ _) -> execJlt m jlt
+      jeq@(JEQ _ _ _) -> execJeq m jeq
+      jgt@(JGT _ _ _) -> execJgt m jgt
+      int@(INT _)     -> execInt m int
+      HLT           -> execHlt m HLT
+  }
+
+execMov m@(Machine ic pc regs mem code hdl) (MOV d s) = do
+  { modifyIORef succ pc
+  ; modifyIORef succ ic
+  ; c  <- readIORef ic
+  ; let reason = if c == 1024 then ResLim else ResNon
+  ; v  <- getValue pc regs mem s
+  ; case d of
+      ArgPC      -> writeIORef pc v >> return (m,reason)
+      ArgReg   r -> writeArray regs r v >> return (m,reason)
+      ArgInd   r -> readArray regs r >>= flip (writeArray mem) v >> return (m,reason)
+      ArgLoc   w -> writeArray mem w v >> return (m,reason)
+      ArgConst _ -> return (m,ResErr)
+  }
+execMov _ _ = error "Not MOV"
+
+execInc m@(Machine ic pc regs mem code hdl) (INC d) = do
+  { modifyIORef succ pc
+  ; modifyIORef succ ic
+  ; c  <- readIORef ic
+  ; let reason = if c == 1024 then ResLim else ResNon
+  ; case d of
+      ArgPC      -> return (m,ResErr)
+      ArgReg   r -> modifyArray succ regs r >> return (m,reason)
+      ArgInd   r -> readArray regs r >>= modifyArray succ mem >> return (m,reason)
+      ArgLoc   w -> modifyArray (1+) mem w >> return (m,reason)
+      ArgConst _ -> return (m,ResErr)
+  }
+execInc _ _ = error "Not INC"
+
+execDec m@(Machine ic pc regs mem code hdl) (DEC d) = do
+  { modifyIORef succ pc
+  ; modifyIORef succ ic
+  ; c  <- readIORef ic
+  ; let reason = if c == 1024 then ResLim else ResNon
+  ; case d of
+      ArgPC      -> return (m,ResErr)
+      ArgReg   r -> modifyArray pred regs r >> return (m,reason)
+      ArgInd   r -> readArray regs r >>= modifyArray pred mem >> return (m,reason)
+      ArgLoc   w -> modifyArray pred mem w >> return (m,reason)
+      ArgConst _ -> return (m,ResErr)
+  }
+execDec _ _ = error "Not DEC"
+
+execAdd m@(Machine ic pc regs mem code hdl) (ADD d s) = do
+  { modifyIORef succ pc
+  ; modifyIORef succ ic
+  ; c  <- readIORef ic
+  ; let reason = if c == 1024 then ResLim else ResNon
+  ; v  <- getValue pc regs mem s
+  ; case d of
+      ArgPC      -> return (m,ResErr)
+      ArgReg   r -> modifyArray (v+) regs r >> return (m,reason)
+      ArgInd   r -> readArray regs r >>= modifyArray (v+) mem >> return (m,reason)
+      ArgLoc   w -> modifyArray (v+) mem w >> return (m,reason)
+      ArgConst _ -> return (m,ResErr)
+  }
+execAdd _ _ = error "Not ADD"
+
+execSub m@(Machine ic pc regs mem code hdl) (SUB d s) = do
+  { modifyIORef succ pc
+  ; modifyIORef succ ic
+  ; c  <- readIORef ic
+  ; let reason = if c == 1024 then ResLim else ResNon
+  ; v  <- getValue pc regs mem s
+  ; case d of
+      ArgPC      -> return (m,ResErr)
+      ArgReg   r -> modifyArray (subtract v) regs r >> return (m,reason)
+      ArgInd   r -> readArray regs r >>= modifyArray (subtract v) mem >> return (m,reason)
+      ArgLoc   w -> modifyArray (subtract v) mem w >> return (m,reason)
+      ArgConst _ -> return (m,ResErr)
+  }
+execSub _ _ = error "Not SUB"
+
+execMul m@(Machine ic pc regs mem code hdl) (MUL d s) = do
+  { modifyIORef succ pc
+  ; modifyIORef succ ic
+  ; c  <- readIORef ic
+  ; let reason = if c == 1024 then ResLim else ResNon
+  ; v  <- getValue pc regs mem s
+  ; case d of
+      ArgPC      -> return (m,ResErr)
+      ArgReg   r -> modifyArray (v*) regs r >> return (m,reason)
+      ArgInd   r -> readArray regs r >>= modifyArray (v*) mem >> return (m,reason)
+      ArgLoc   w -> modifyArray (v*) mem w >> return (m,reason)
+      ArgConst _ -> return (m,ResErr)
+  }
+execMul _ _ = error "Not MUL"
+
+execDiv m@(Machine ic pc regs mem code hdl) (DIV d s) = do
+  { modifyIORef succ pc
+  ; modifyIORef succ ic
+  ; c  <- readIORef ic
+  ; let reason = if c == 1024 then ResLim else ResNon
+  ; v  <- getValue pc regs mem s
+  ; case d of
+      ArgPC      -> return (m,ResErr)
+      ArgReg   r -> if v==0 then return (m,ResErr) else modifyArray (`div` v) regs r >> return (m,reason)
+      ArgInd   r -> if v==0 then return (m,ResErr) else readArray regs r >>= modifyArray (`div` v) mem >> return (m,reason)
+      ArgLoc   w -> if v==0 then return (m,ResErr) else modifyArray (`div` v) mem w >> return (m,reason)
+      ArgConst _ -> return (m,ResErr)
+  }
+execDiv _ _ = error "Not DIV"
+
+execAnd m@(Machine ic pc regs mem code hdl) (AND d s) = do
+  { modifyIORef succ pc
+  ; modifyIORef succ ic
+  ; c  <- readIORef ic
+  ; let reason = if c == 1024 then ResLim else ResNon
+  ; v  <- getValue pc regs mem s
+  ; case d of
+      ArgPC      -> return (m,ResErr)
+      ArgReg   r -> modifyArray (v .&.) regs r >> return (m,reason)
+      ArgInd   r -> readArray regs r >>= modifyArray (v .&.) mem >> return (m,reason)
+      ArgLoc   w -> modifyArray (v .&.) mem w >> return (m,reason)
+      ArgConst _ -> return (m,ResErr)
+  }
+execAnd _ _ = error "Not ADD"
+
+execOr m@(Machine ic pc regs mem code hdl) (OR d s) = do
+  { modifyIORef succ pc
+  ; modifyIORef succ ic
+  ; c  <- readIORef ic
+  ; let reason = if c == 1024 then ResLim else ResNon
+  ; v  <- getValue pc regs mem s
+  ; case d of
+      ArgPC      -> return (m,ResErr)
+      ArgReg   r -> modifyArray (v .|.) regs r >> return (m,reason)
+      ArgInd   r -> readArray regs r >>= modifyArray (v .|.) mem >> return (m,reason)
+      ArgLoc   w -> modifyArray (v .|.) mem w >> return (m,reason)
+      ArgConst _ -> return (m,ResErr)
+  }
+execOr _ _ = error "Not OR"
+
+execXor m@(Machine ic pc regs mem code hdl) (XOR d s) = do
+  { modifyIORef succ pc
+  ; modifyIORef succ ic
+  ; c  <- readIORef ic
+  ; let reason = if c == 1024 then ResLim else ResNon
+  ; v  <- getValue pc regs mem s
+  ; case d of
+      ArgPC      -> return (m,ResErr)
+      ArgReg   r -> modifyArray (v `xor`) regs r >> return (m,reason)
+      ArgInd   r -> readArray regs r >>= modifyArray (v `xor`) mem >> return (m,reason)
+      ArgLoc   w -> modifyArray (v `xor`) mem w >> return (m,reason)
+      ArgConst _ -> return (m,ResErr)
+  }
+execXor _ _ = error "Not XOR"
+
+execJlt m@(Machine ic pc regs mem code hdl) (JLT targ x y) = do
+  { modifyIORef succ pc
+  ; modifyIORef succ ic
+  ; c  <- readIORef ic
+  ; let reason = if c == 1024 then ResLim else ResNon
+  ; vx  <- getValue pc regs mem x
+  ; vy  <- getValue pc regs mem y
+  ; if vx < vy then modifyIORef (const (getValue targ)) pc >> return (m,ResNon)
+               else return (m,ResNon)
+  }
+execJlt _ _ = error "Not JLT"
+
+execJeq m@(Machine ic pc regs mem code hdl) (JEQ targ x y) = do
+  { modifyIORef succ pc
+  ; modifyIORef succ ic
+  ; c  <- readIORef ic
+  ; let reason = if c == 1024 then ResLim else ResNon
+  ; vx  <- getValue pc regs mem x
+  ; vy  <- getValue pc regs mem y
+  ; if vx == vy then modifyIORef (const (getValue targ)) pc >> return (m,ResNon)
+                else return (m,ResNon)
+  }
+execJEQ _ _ = error "Not JEQ"
+
+execJgt m@(Machine ic pc regs mem code hdl) (JGT targ x y) = do
+  { modifyIORef succ pc
+  ; modifyIORef succ ic
+  ; c  <- readIORef ic
+  ; let reason = if c == 1024 then ResLim else ResNon
+  ; vx  <- getValue pc regs mem x
+  ; vy  <- getValue pc regs mem y
+  ; if vx > vy then modifyIORef (const (getValue targ)) pc >> return (m,ResNon)
+               else return (m,ResNon)
+  }
+execJgt _ _ = error "Not JGT"
+
+execInt m@(Machine ic pc regs mem code hdl) (Int i) = do
+  { modifyIORef succ pc
+  ; modifyIORef succ ic
+  ; c  <- readIORef ic
+  ; let reason = if c == 1024 then ResLim else ResNon
+  ; readArray hdl i >>= flip ($) regs
+  ; return (m,ResNon)
+  }
+execInt _ _ = error "Not Int"
+
+execHlt m@(Machine ic _ _ _ _) HLT = modifyIORef' succ ic >> return (m,ResHLT)
+execHlt _ _ = error "not HLT"
+
+getValue :: IORef Word8 -> IOArray Reg Word8 -> IOArray Word8 Word8 -> Arg -> IO Word8
+getValue pc _    _   ArgPC       = readIORef pc
+getValue _ regs _   (ArgReg r)   = readArray regs r
+getValue _ regs mem (ArgInd r)   = readArray regs r >>= readArray mem
+getValue _ _    _   (ArgConst w) = return w
+getValue _ _    mem (ArgConst w) = readArray mem w
+
+modifyArray :: (MArray a e m, Ix i) => (e -> e) -> a i e -> i -> m ()
+modifyArray f a i = readArray a i >>= writeArray a i . f
