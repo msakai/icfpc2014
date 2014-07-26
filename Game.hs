452ff16d2e2ac0361fdaa45b16b01acb57000bd7
Masahiro Sakai <masahiro.sakai@gmail.com>
2014-07-26 17:08:48 +0900

    add some definitions to Game.hs

--- Game.hs
+++ Game.hs
@@ -14,3 +14,22 @@ computeLevel :: (Width,Height) -> Level
 computeLevel (width,height) = ceiling $ fromIntegral (width * height) / (100::Rational)
 -- computeLevel (15,18) == 3
 
+type UTC = Integer
+
+utcEOL :: (Width,Height) -> UTC
+utcEOL (w,h) = 127*w*h*16
+
+utcFruit1Appear :: UTC
+utcFruit1Appear = 127*200 
+
+utcFruit2Appear :: UTC
+utcFruit2Appear = 127*400
+
+utcFruit1Expire :: UTC
+utcFruit1Expire = 127*280
+
+utcFruit2Expire :: UTC
+utcFruit2Expire = 127*480
+
+frightModeDuration :: UTC
+frightModeDuration = 127*20
