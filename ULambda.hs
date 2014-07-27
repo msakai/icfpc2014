000717c30bd5f577d8f7153bf8299eece1f11ff3
Nobuo Yamashita <nobsun@sampou.org>
2014-07-27 17:21:42 +0900

    make TopLevelFuncDefinition Show

--- ULambda.hs
+++ ULambda.hs
@@ -55,7 +55,7 @@ data TopLevelFuncDefinition
   , funcParams  :: [Ident]
   , funcBody    :: Expr
   }
-
+  deriving (Show)
 -- ---------------------------------------------------------------
 -- EDSL combinators
 -- ---------------------------------------------------------------
